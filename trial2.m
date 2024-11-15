ecb16a72767c03668605eb99759443639962b9a0
Sang Woo Hong <74524326+dhkdsns20@users.noreply.github.com>
2024-11-15 15:48:37 +0900

    Add files via upload

--- trial2.m
+++ trial2.m
@@ -1,25 +1,21 @@
-% trial 2: y(n) = x(n)-x(n-2)+0.95*y(n-1)-0.9025*y(n-2)
+% trial2
 clc; close all;
-% define w be the interval of [-pi, pi] with 300 steps
-w = [-300:300]*pi/300; 
-% set a be the constant values for each y
-a = [1 -0.95 0.9025]; 
-% set b be the constant values for x
-b = [1 0 -1]; 
-% define H be frequency response function
-[H] = freqresp(b,a,w); 
-% define magH be magnitude of H
-magH = abs(H); 
-% define phaH be phase of H
-phaH = angle(H)*180/pi;
+% H(z) = (z+1)/(z-0.5)
+% impulse representation
+b1 = [1 1 0]; a1 = [1 -0.5 0];
+[R1,p1,c1] = residuez(b1(1:2),a1(1:2));
+Mp = abs(p1);
+Ap = angle(p1)'/pi;
+% H(z) = 3/(1-0.5*z^(-1))
+% h(n) = 3*0.5^(n)u(n)
 
-% figure
+% difference equation representation
+% H(z) = Y/X = (1+z^(-1))/(1-0.5*z^(-1))
+% Y*(1 - 0.5*z^(-1)) = X*(1+z^(-1))
+% Y -0.5*z^(-1)*Y = X + z^(-1)*X
+% y(n) - 0.5y(n-1) = x(n) + x(n-1)
+
+% pole-zero plot
 Hf_1 = figure;
-% magnitude plot
-subplot(2,1,1); plot(w/pi,magH,'LineWidth',1.5); axis([-1 1 0 25]);
-xlabel('\omega / \pi'); ylabel('|H|');
-title('Magnitude response');
-% phase plot
-subplot(2,1,2); plot(w/pi,phaH,'LineWidth',1.5); axis([-1 1 -200 200]);
-xlabel('\omega / \pi'); ylabel('Degrees');
-title('Phase response');
+[Hz,Hp,Hl] = zplane(b1,a1);
+title('Pole-Zero plot');
