0103e9f0277ece9fc968123fb3064c424dbd7407
Sang Woo Hong <74524326+dhkdsns20@users.noreply.github.com>
2024-12-01 21:30:15 +0900

    Add files via upload

---
+++ HW8_2b.m
@@ -0,0 +1,30 @@
+% (b) N = 50
+N = 50;
+Fs = 100;               % 샘플링 주파수
+t_step = 1 / Fs;        % 샘플링 간격 : 0.01
+t = (0:N-1) * t_step; 
+
+% Signal generation
+f1 = 2; % sin의 주파수
+f2 = 4; % cos의 주파수
+x = 2 * sin(2 * pi * f1 * t) + 5 * cos(2 * pi * f2 * t); % xa(t) 신호 정의
+
+
+% DFT computation
+X = fft(x, N);
+
+% Plot Real part
+figure;
+stem(0:N-1, real(X), 'b');
+title('Real Part of DFT (b) N=50');
+xlabel('k');
+ylabel('Real(X(k))');
+grid on;
+
+% Plot Imaginary part
+figure;
+stem(0:N-1, imag(X), 'r');
+title('Imaginary Part of DFT (b) N=50');
+xlabel('k');
+ylabel('Imag(X(k))');
+grid on;
